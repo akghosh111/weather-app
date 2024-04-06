8252e289aa44960027bf176e7ab28e43d5a7ec3e
akghosh111 <ghoshkiran2017@gmail.com>
2024-04-06 19:38:35 +0530

    integrated weather api with js, layout bootstrao

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '',
+            'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+        }
+    };
+
+    const getWeather = async (city) => {
+        cityName.innerHTML = city
+        try {
+            const response = await fetch(`${url}?city=${city}`, options);
+            const result = await response.json();
+            console.log(result);
+
+            cloud_pct.innerHTML = result.cloud_pct;
+            temp.innerHTML = result.temp;
+            feels_like.innerHTML = result.feels_like;
+            humidity.innerHTML = result.humidity;
+            min_temp.innerHTML = result.min_temp;
+            max_temp.innerHTML = result.max_temp;
+            wind_speed.innerHTML = result.wind_speed;
+            wind_degrees.innerHTML = result.wind_degrees;
+            sunrise.innerHTML = result.sunrise;
+            sunset.innerHTML = result.sunset;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const submit = document.getElementById("submit");
+    const cityInput = document.getElementById("city");
+
+    submit.addEventListener("click", (e) => {
+        e.preventDefault();
+        const city = cityInput.value;
+        getWeather(city);
+    });
+
+    // Default to getting weather for Delhi on page load
+    getWeather("Delhi");
+});
