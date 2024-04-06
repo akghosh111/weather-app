4f69b5a67f40d44318f5f29e6e1a54a94f7bfaaf
akghosh111 <ghoshkiran2017@gmail.com>
2024-04-06 20:41:16 +0530

    updated layout with proper units

--- script.js
+++ script.js
@@ -18,11 +18,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
             cloud_pct.innerHTML = result.cloud_pct;
             temp.innerHTML = result.temp;
+            temp2.innerHTML = result.temp;
             feels_like.innerHTML = result.feels_like;
             humidity.innerHTML = result.humidity;
+            humidity2.innerHTML = result.humidity;
             min_temp.innerHTML = result.min_temp;
             max_temp.innerHTML = result.max_temp;
             wind_speed.innerHTML = result.wind_speed;
+            wind_speed2.innerHTML = result.wind_speed;
             wind_degrees.innerHTML = result.wind_degrees;
             sunrise.innerHTML = result.sunrise;
             sunset.innerHTML = result.sunset;
